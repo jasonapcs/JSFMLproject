77244413713f99d8a8decffa3f5b2b17729af345
jzzhang <jzzhang@PLD-LAB-400-13T.fayette.ketsds.net>
2018-09-10 10:29:06 -0400

    Now renders bigfoot

--- JSFMLproject/src/mainpackage/Main.java
+++ JSFMLproject/src/mainpackage/Main.java
@@ -1,23 +1,42 @@
 package mainpackage;
 
+import java.io.*;
+import java.nio.file.Paths;
+
 import org.jsfml.graphics.*;
 import org.jsfml.window.*;
 import org.jsfml.window.event.*;
 
+import java.util.Scanner;
+
 public class Main {
 
+	static Scanner scanner = new Scanner(System.in);
+	
 	public static RenderWindow app;
-
+	
 	
 	public static void main(String[] args) {
-
+		
 		app = new RenderWindow(VideoMode.getDesktopMode(), "JSFML project");
 		
 		app.setFramerateLimit(60);
 		
-		// load up things, etc.
+		// Declare textures and sprites
+		Texture BigfootFrontTexture = new Texture();
+		Sprite Bigfoot_front;
 		
-
+		// load up things, etc.
+		try {
+			BigfootFrontTexture.loadFromFile(Paths.get("resources/bigfoot_front.png"));
+			Bigfoot_front = new Sprite(BigfootFrontTexture);
+		}
+		catch (IOException e){
+			e.printStackTrace();
+			app.close();
+			scanner.nextLine();
+			return;
+		}
 		
 		// Main Loop
 		while (app.isOpen()){
@@ -33,9 +52,12 @@ public class Main {
 			app.clear(Color.WHITE);
 			
 			// Draw things
+			app.draw(Bigfoot_front);
+			
 			
 			app.display();
 		}
+		
+		scanner.nextLine();
 	}
-
 }
